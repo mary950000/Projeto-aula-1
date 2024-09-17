444e409538565ec2fdbfdac10eb4b7f9eecf8697
Mary <00001115266883sp@al.educacao.sp.gov.br>
2024-09-17 08:06:31 -0300

    Projeto aula 1

---
+++ eketco.js
@@ -0,0 +1,18 @@
+function setup() {draw() {
+  
+    stroke ("blue");
+    createCanvas(600, 600);
+     background("white");
+  }
+  
+  
+  function 
+    fill ("red");
+   
+    
+    // console.log(mouseIsPressed);
+    
+    if (mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
